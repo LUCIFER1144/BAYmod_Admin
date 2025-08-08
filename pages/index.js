6e3a51ed7b7402fabd0af6b2c01512146394f9c8
LUCIFER1144 <135417247+LUCIFER1144@users.noreply.github.com>
2025-08-08 18:13:56 +0300

    setting up the database with mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
+import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
-  return (
-    
+  const { data: session } = useSession()
+  if(!session) {
+    return (
       <div className="bg-blue-900 w-screen h-screen flex items-center">
         <div className="text-center w-full">
-          <button className="bg-white p-2 px-4 rounded-lg">Sign in</button>
+          <button onClick={() =>signIn('google')} className="bg-white p-2 px-4 rounded-lg">Sign in</button>
         </div>
       </div>
-  )
+    );
+  }
+
+  return (
+    <div>logged in {session.user.email} </div>
+  );
 }
