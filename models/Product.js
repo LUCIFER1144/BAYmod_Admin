665b764092cb4c76966dfb2d14fb67387acfe7eb
Bader-ed <edbdr510@gmail.com>
2025-08-16 03:34:26 +0300

    edit & delete product..

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,12 @@
-import { model } from "mongoose";
-import {Schema} from "mongoose";
+import mongoose, {model, Schema, models} from "mongoose";
 
 const ProductSchema = new Schema({
-    title: {type: String, required:true},
-    description: String,
-    price: {type: Number, required:true},
+  title: {type:String, required:true},
+  description: String,
+  price: {type: Number, required: true},
+  // images: [{type:String}],
+  // category: {type:mongoose.Types.ObjectId, ref:'Category'},
+  // properties: {type:Object},
 });
 
-export const Product = model('Product',ProductSchema);
+export const Product = models.Product || model('Product', ProductSchema);
